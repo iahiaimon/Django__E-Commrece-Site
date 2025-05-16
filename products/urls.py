6e96c97070541909e291b49023aebf2541f58bd0
iahia <iahiaimon555@gmail.com>
2025-05-16 16:51:19 +0600

    Updating details page

--- products/urls.py
+++ products/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
 
     path('category/<slug:slug>/', views.category_products, name='category_products'),
     path('product_details/<slug:slug>/' , views.product_details , name="product_details"),
+    # path('add_to_cart' , views.product_details , name="add_to_cart"),
+    # path('buy_now' , views.product_details , name="buy_now"),
 
 
 ]
